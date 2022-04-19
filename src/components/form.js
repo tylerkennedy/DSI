127ee6508ff3040d9e52c387c4a680d24c12c0b8
Dillon <43819022+Dcode99@users.noreply.github.com>
2022-04-19 13:38:32 -0400

    Update form entries

--- src/components/form.js
+++ src/components/form.js
@@ -31,8 +31,12 @@ class Form extends Component {
       // Get data from the form.
       // Create a property for each form field
       const data = {
+        coronerOrDeputy: event.target.coronerOrDeputy.value,
+        county: event.target.county.value,
         firstName: event.target.firstName.value,
         lastName: event.target.lastName.value,
+        address: event.target.address.value,
+        city: event.target.city.value
       }
 
       console.log("Here is our form data: ", data) // Look for this output in the browser console to see if the data is being read properly
@@ -42,12 +46,28 @@ class Form extends Component {
       <Flex>
         <form onSubmit={handleSubmit}>
           <Box py={2}>
+            <FormLabel htmlFor='coronerOrDeputy'>First name</FormLabel>
+            <Input id='coronerOrDeputy' type='text'></Input>
+          </Box>      
+          <Box py={2}>
+          <Box py={2}>
+            <FormLabel htmlFor='county'>First name</FormLabel>
+            <Input id='county' type='text'></Input>
+          </Box>
             <FormLabel htmlFor='firstName'>First name</FormLabel>
             <Input id='firstName' type='text'></Input>
           </Box>
           <Box py={2}>
             <FormLabel htmlFor='lastName'>Last name</FormLabel>
             <Input id='lastName' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='address'>First name</FormLabel>
+            <Input id='address' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='city'>First name</FormLabel>
+            <Input id='city' type='text'></Input>
           </Box>
           <Button mt={4} type="submit">
             Submit
