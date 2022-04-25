fdc931fcc808593212e8256a4de3471e32005119
tylerkennedy <kennedytyler25@icloud.com>
2022-04-25 10:33:18 -0400

    Fix form style and create alert on submit

--- src/components/form.js
+++ src/components/form.js
@@ -4,6 +4,7 @@ import {
   Button,
   Flex,
   Heading,
+  Text,
   Link,
   FormControl,
   FormLabel,
@@ -15,9 +16,14 @@ import {
 import { AddIcon, StarIcon } from '@chakra-ui/icons'
 
 const styles = {
-
+  width: 550
 };
 
+// // import needed tools for connecting to the database
+// import { getDatabase, ref, set } from "firebase/database";
+
+// // given from firebase doc's
+// firebase.initializeApp(config); //Initialize your firebase here passing your firebase account config object
 
 
 class Form extends Component {
@@ -36,11 +42,67 @@ class Form extends Component {
       }
 
       console.log("Here is our form data: ", data) // Look for this output in the browser console to see if the data is being read properly
+
+      // do conversion for sending
+      var prep = JSON.stringify(data);
+      //^ also makes sure no disallowed values are in place
+
+      // the docs define this as best practices to push with unique key each time
+      // const newPostRef = postsRef.push();
+      // newPostRef.set(prep);
+
+      event.target.reset();
+      alert("Form uploaded!")
     }
 
     return (
-      <Flex>
-        <form onSubmit={handleSubmit}>
+      <Flex flexDir={'column'}>
+        <Heading as={'h2'} pb={4}>Upload a Death Scene Investigation</Heading>
+        <form onSubmit={handleSubmit} style={styles}>
+          {/* // Start Boxes of Information
+          // Basic Info Section */}
+          <Box py={2}>
+            <FormLabel htmlFor='caseNumber'>Case Number</FormLabel>
+            <Input id='caseNumber' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='coronerOrDeputy'>Coroner/Deputy</FormLabel>
+            <Input id='coronerOrDeputy' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='dateOfCall'>Date of Call</FormLabel>
+            <Input id='dateOfCall' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='timeOfCall'>Time of Call</FormLabel>
+            <Input id='timeOfCall' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='personCalling'>Person Calling</FormLabel>
+            <Input id='personCalling' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='policeAgency'>Police Agency</FormLabel>
+            <Input id='policeAgency' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='county'>County</FormLabel>
+            <Input id='county' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='timeOfArrival'>Time of Arrival</FormLabel>
+            <Input id='timeOfArrival' type='text'></Input>
+          </Box>
+          {/* // Possibly change to checkbox format */}
+          <Box py={2}>
+            <FormLabel htmlFor='suspectedCause'>Suspected Cause</FormLabel>
+            <Input id='suspectedCause' type='text'></Input>
+          </Box>
+          {/* // Begin Decedent Information Section */}
+          <Box py={2}>
+            <FormLabel htmlFor='coronerOrDeputy'>Coroner/Deputy</FormLabel>
+            <Input id='coronerOrDeputy' type='text'></Input>
+          </Box>
           <Box py={2}>
             <FormLabel htmlFor='firstName'>First name</FormLabel>
             <Input id='firstName' type='text'></Input>
@@ -49,11 +111,121 @@ class Form extends Component {
             <FormLabel htmlFor='lastName'>Last name</FormLabel>
             <Input id='lastName' type='text'></Input>
           </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='maleOrFemale'>Male or Female</FormLabel>
+            <Input id='maleOrFemale' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='address'>Address</FormLabel>
+            <Input id='address' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='city'>City</FormLabel>
+            <Input id='city' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='countyOfResidence'>County of Residence</FormLabel>
+            <Input id='countyOfResidence' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='stateOfResidence'>State of Residence</FormLabel>
+            <Input id='stateOfResidence' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='zip'>Zip</FormLabel>
+            <Input id='zip' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='ssn'>SSN#</FormLabel>
+            <Input id='ssn' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='age'>Age</FormLabel>
+            <Input id='age' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='survivingSpouse'>Surviving Spouse</FormLabel>
+            <Input id='survivingSpouse' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='dateOfBirth'>Date of Birth</FormLabel>
+            <Input id='dateOfBirth' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='mothersMaidenName'>Mother's Maiden Name</FormLabel>
+            <Input id='mothersMaidenName' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='birthPlace'>Birth Place</FormLabel>
+            <Input id='birthPlace' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='fathersName'>Father's Name</FormLabel>
+            <Input id='fathersName' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='height'>Height (Inches)</FormLabel>
+            <Input id='height' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='weight'>Weight (Pounds)</FormLabel>
+            <Input id='weight' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='eyes'>Eyes</FormLabel>
+            <Input id='eyes' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='hair'>Hair</FormLabel>
+            <Input id='hair' type='text'></Input>
+          </Box>
+          {/* // Still required: checkbox status sections (marital status, education, race, ethnicity, employment, veteran or pregnant)
+          // Begin Next of Kin Section */}
+          <Box py={2}>
+            <FormLabel htmlFor='notifiedBy'>Notified By</FormLabel>
+            <Input id='notifiedBy' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='dateNotified'>Date Notified</FormLabel>
+            <Input id='dateNotified' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='timeNotified'>Time Notified</FormLabel>
+            <Input id='timeNotified' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='nameKin'>Name</FormLabel>
+            <Input id='nameKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='addressKin'>Address</FormLabel>
+            <Input id='addressKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='phoneNumberKin'>Phone Number</FormLabel>
+            <Input id='phoneNumberKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='relationshipKin'>Relationship</FormLabel>
+            <Input id='relationshipKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='cityKin'>City</FormLabel>
+            <Input id='cityKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='stateKin'>State</FormLabel>
+            <Input id='stateKin' type='text'></Input>
+          </Box>
+          <Box py={2}>
+            <FormLabel htmlFor='zipKin'>Zip</FormLabel>
+            <Input id='zipKin' type='text'></Input>
+          </Box>
           <Button mt={4} type="submit">
             Submit
           </Button>
         </form>
-      </Flex>
+      </Flex >
     );
   }
 }
